412a2d8d587dfe025b7565fa810235c7b4b00aaf
gregorterrill <heygregor@gmail.com>
2013-10-16 13:03:50 -0400

    modifications to LESS layout

--- home.php
+++ home.php
@@ -8,25 +8,25 @@
 
 <section class="page-homepage">
 
-	<div class="grid">
+	<div class="grid-row">
 		<div class="grid-3">.grid-3</div>
 		<div class="grid-3">.grid-3</div>
 		<div class="grid-3">.grid-3</div>
 		<div class="grid-3">.grid-3</div>
 	</div>
 
-	<div class="grid">
+	<div class="grid-row">
 		<div class="grid-4">.grid-4</div>
 		<div class="grid-4">.grid-4</div>
 		<div class="grid-4">.grid-4</div>
 	</div>
 
-	<div class="grid">
+	<div class="grid-row">
 		<div class="grid-6">.grid-6</div>
 		<div class="grid-6">.grid-6</div>
 	</div>
 
-	<div class="grid">
+	<div class="grid-row">
 		<div class="grid-8">.grid-8</div>
 		<div class="grid-2">.grid-2</div>
 	</div>
--- page.php
+++ page.php
@@ -13,7 +13,7 @@
 
 <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 
-	<section class="page-<?php the_ID(); ?>">
+	<section class="page-container page-<?php the_ID(); ?>">
 		<h1><?php the_title(); ?></h1>
 
 		<?php the_content(); ?>
